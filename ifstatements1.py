eef2671dc9cb0cbb33b72f7532c94314b0d370c8
E2415-Matt <ivymatt2017@gmail.com>
2020-07-07 22:52:52 +0100

    this is firt commit of Assignment - 1 (if-Statements)

---
+++ ifstatements1.py
@@ -0,0 +1,10 @@
+name = (input("Please enter your name: "))
+
+if 'Matt' == name:
+
+    print('Hello, ' + name + '! The password is: W@12')
+
+else:
+
+    print('Hello, ' + name + '! See you later.')
+
