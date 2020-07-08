be06d1cd396b2aced0de96b5fdc72fe8acc8eaba
E2415-Matt <ivymatt2017@gmail.com>
2020-07-08 19:39:28 +0100

    this is the final version of covid 19 assignment

--- assignment_2_covid_19.py
+++ assignment_2_covid_19.py
@@ -1,23 +1,20 @@
-age = input('Are you a cigarette addict older than 75 years old? (Yes or No)')
-age.capitalize()
+age = str(input('Are you a cigarette addict older than 75 years old? (Yes/No):')).title().strip()
 if age == 'Yes':
     age = True
 else:
     age = False
-chronic = input('Do you have a severe chronic disease? (Yes or No)')
-chronic.capitalize()
+chronic = str(input('Do you have a severe chronic disease? (Yes/No):')).title().strip()
 if chronic == 'Yes':
     chronic = True
 else:
     chronic = False
-immune = input ('Is your immune system too weak? (Yes or No)')
-immune.capitalize()
+immune = str(input('Is your immune system too weak? (Yes/No):')).title().strip()
 if immune == 'Yes':
     immune = True
 else:
     immune = False
 risk = age or chronic or immune
-if risk == (True):
+if risk == True:
     print('You are in risky group.')
 else:
     print('You are not in risky group.')
