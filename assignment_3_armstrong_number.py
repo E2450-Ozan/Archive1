94852df397c51381488114a419de88583daa4aa6
E2415-Matt <ivymatt2017@gmail.com>
2020-07-11 12:24:40 +0100

    final commit for armstrong number

--- assignment_3_armstrong_number.py
+++ assignment_3_armstrong_number.py
@@ -1,19 +1,30 @@
-num = int(input("Please enter a positive number: "))
-sum = 0
-temp = num
-converted_num = str(num)
-n = (len(converted_num))
-if num < 0:
-    print("You have entered an invalid value.")
-elif type(num) != int:
-    print("You have entered an invalid value.")
-else:
-    while temp >0:
-        digit = temp % 10
-        sum += digit ** n
-        temp //= 10
-if num == sum:
-    print(num, "is an Armstrong number.")
-else:
-    print(num, "is not an Armstrong number.")
+entry = input("Please enter a positive number: ")
+lst1 = list(entry)
+lst2 = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+a = True
+for i in lst1:
+    if i not in lst2:
+        print("It is an invalid entry. Don't use non-numeric, float, or negative values!")
+        a = False
+        break
+while a:
+    num = int(entry)
+    if num >= 0:
+        num = int(num)
+        sum = 0
+        temp = num
+        converted_num = str(num)
+        n = (len(converted_num))
+        while temp > 0:
+            digit = temp % 10
+            sum += digit ** n
+            temp //= 10
+        if num == sum:
+            print(num, "is an Armstrong number.")
+            break
+        else:
+            print(num, "is not an Armstrong number.")
+            break
+    else:
+        print("It is an invalid entry. Don't use non-numeric, float, or negative values!")
 
