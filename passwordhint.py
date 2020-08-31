f6506cdb2ae113369d98028155a02b0740104ce7
E2415Matt <67641230+E2415Matt@users.noreply.github.com>
2020-08-31 08:29:04 +0100

    Delete passwordhint.py

--- passwordhint.py
+++
@@ -1,5 +0,0 @@
-name = (input("Please enter memorable information: "))
-if name == 'Matt'
-    print('Hello, ' + name + '! The password is: H@W1ucky!12')
-else:
-    print('Hello, ' + name + '! See you later.')
